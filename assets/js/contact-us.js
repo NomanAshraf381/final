4bd8c50b32193b3a2b8afc19d8b2b166a7282739
NomanAshraf381 <139213039+NomanAshraf381@users.noreply.github.com>
2023-09-14 14:08:12 +0500

    Update contact-us.js

--- assets/js/contact-us.js
+++ assets/js/contact-us.js
@@ -1,3 +1,19 @@
+const ourIframe = document.getElementById('iframe-container');
+            setTimeout(() => {
+            const ourIframe = 
+            `<iframe 
+                src = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3444.9655054271157!2d71.9157354145998!3d30.29504361364653!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x393b554b068b1b21%3A0x7517d8adc887c625!2sDigital%20Applications!5e0!3m2!1sen!2s!4v1677411866697!5m2!1sen!2s'
+                width = '600' 
+                height = '450' 
+                style.border = '0' 
+                title = 'Our Location' 
+                allowfullscreen = true 
+                referrerpolicy = 'no-referrer-when-downgrade' >
+            </iframe>`
+            ourIframe.innerHTML = ourIframe;
+            }, 7000);
+
+
 // send email 
 let messageButton = document.querySelector('#sendmess');
 messageButton.addEventListener('click', function (event) {
